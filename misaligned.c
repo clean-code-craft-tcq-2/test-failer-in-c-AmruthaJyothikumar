361d0c9009d067b4a6cceae1b5b8037bd4b83df3
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2022-01-25 16:28:31 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -15,7 +15,18 @@ int printColorMap() {
 
 int main() {
     int result = printColorMap();
-    assert(result == 25);
+    int spacecount;
+    char str[100];
+    assert(result == 25);  
+    for(i = 0; i < 5; i++) {
+        for(j = 0; j < 5; j++) {
+            sprintf(str,"%d | %s | %s\n",i * 5 + j, majorColor[i], minorColor[i]);
+            for(k=0; str[k] != '|'; k++){
+                spacecount++;
+                }
+        }
+    }
+    assert(spacecount == 2);
     printf("All is well (maybe!)\n");
     return 0;
 }
